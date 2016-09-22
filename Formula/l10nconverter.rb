127fea9b2cb22c921f29a77a1668b7c902138a2a
Liquidsoul <liquidsoul@liquidsoul.fr>
2016-09-22 08:35:03 +0200

    [l10nconverter] version 0.0.5
    Change install process because the tool is now built using the
    Swift Package Manager and we need to change the binary name here.

--- Formula/l10nconverter.rb
+++ Formula/l10nconverter.rb
@@ -1,12 +1,12 @@
 class L10nconverter < Formula
   desc "A command-line tool to convert mobile localization files."
   homepage "https://github.com/Liquidsoul/LocalizationConverter"
-  url "https://github.com/Liquidsoul/LocalizationConverter.git", :tag => "0.0.4"
+  url "https://github.com/Liquidsoul/LocalizationConverter.git", :tag => "0.0.5"
   head "https://github.com/Liquidsoul/LocalizationConverter.git", :branch => "develop"
 
   def install
     system "make", "release"
-    bin.install "release.xcarchive/Products/usr/local/bin/l10nconverter"
+    bin.install ".build/release/LocalizationConverter" => "l10nconverter"
   end
 
   test do
