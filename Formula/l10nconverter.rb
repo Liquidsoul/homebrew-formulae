aeb1f67cf3db663b234aa63711326da711b9f4a2
Liquidsoul <liquidsoul@liquidsoul.fr>
2016-09-24 08:55:14 +0200

    [l10nconverter] version 0.0.6
    Change the install process because now the commandline binary has the
    correct name, we do not need to rename it.

--- Formula/l10nconverter.rb
+++ Formula/l10nconverter.rb
@@ -1,15 +1,25 @@
 class L10nconverter < Formula
   desc "A command-line tool to convert mobile localization files."
   homepage "https://github.com/Liquidsoul/LocalizationConverter"
-  url "https://github.com/Liquidsoul/LocalizationConverter.git", :tag => "0.0.5"
+  url "https://github.com/Liquidsoul/LocalizationConverter.git", :tag => "0.0.6"
   head "https://github.com/Liquidsoul/LocalizationConverter.git", :branch => "develop"
 
   def install
     system "unset CC; make release"
-    bin.install ".build/release/LocalizationConverter" => "l10nconverter"
+    bin.install ".build/release/l10nconverter"
   end
 
   test do
-    system "#{bin}/l10nconverter", "help"
+    (testpath/"strings.xml").write <<-EOS.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <resources>
+        <string name="app.name">Todo</string>
+        <string name="Loading">Loading…</string>
+        <string name="Yes">Yes</string>
+        <string name="No">No</string>
+        <string name="Congratulations">Congratulations "<b>%1$s</b>"! You've done it!</string>
+      </resources>
+    EOS
+    system "#{bin}/l10nconverter", "convertAndroidFile", "strings.xml"
   end
 end
